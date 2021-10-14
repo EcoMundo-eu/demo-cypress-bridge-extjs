7ef79a67bfb28f44abe600f84f2e8e2bd66b7a51
Jeho Benjamina <bravelonjanahary@bocasay.com>
2021-10-14 11:34:56 +0300

    Live code!

--- cypress/integration/Coworkee/newOrganisation.spec.js
+++ cypress/integration/Coworkee/newOrganisation.spec.js
@@ -1,5 +1,50 @@
 describe('New organization', function () {
     it('should create a new organization', function () {
+        // Enter in the app
+        cy.visit('http://localhost:3000')
+
+        // Click login button
+        cy.getButtonByTestId('logInButton')
+            .clickButton()
+
+        // Click organization menu
+        cy.get('[data-menu-cy="organizationsMenu"]')
+            .parent()
+            .click({force: true})
+
+        // Click create button
+        cy.getButtonByTestId('createOrganizationButton')
+            .clickButton()
+
+        // New organization information
+        const newOrganization = {
+            name: 'jsdays',
+            manager: 'Benjamin'
+        }
+
+        // Write the organization name
+        cy.getInputByTestId('organisationNameInput')
+            .writeTextInput(newOrganization.name)
+
+        // Select the manager name by display field in combobox
+        cy.getComboboxByTestId('managerCombobox')
+            .selectComboboxItemByDisplayField(newOrganization.manager)
+
+        // Click create button
+        cy.getButtonByTestId('submitCreateForm')
+            .clickButton()
+
+        // The popup should be disappear
+        cy.get('[data-cy=organizationCreateModal]')
+            .should('not.exist')
+
+        // Click refresh grid button
+        cy.getButtonByTestId('refreshButton')
+            .clickButton()
+
+        // Select the row which holds the key `name` with value of new organization
+        cy.getGridByTestId('organizationGrid')
+            .selectGridRowByKeyValue('name', newOrganization.name)
 
     });
 });
--- cypress/integration/Coworkee/withAndWithout.spec.js
+++ cypress/integration/Coworkee/withAndWithout.spec.js
@@ -1,10 +1,27 @@
 describe('Demo with and without the testing library', function () {
-    it('should select the row which contains `Benjamina` value', function () {
+    it('should select the row which contains `Benjamin` value', function () {
+
+        // Enter in the app
+        cy.visit('http://localhost:3000')
+
+        // Interact manually the application
+        cy.pause()
 
         // Without testing library
-
+        /*cy.get('[data-cy="employeesGrid"]')
+            .within(function () {
+                cy.contains('Benjamin')
+                    .parent()
+                    .parent()
+                    .parent()
+                    .parent()
+                    .dblclick()
+            })*/
 
         // With testing library
+        cy.getGridByTestId('employeesGrid')
+            .selectGridRowByKeyValue('firstname', 'Benjamin')
+            .dblclickSelectedRow()
 
     });
 });
